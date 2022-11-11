a2cc0907f530a49e63a2cb6dfd08a49074e2338e
abinbs <abinbinusam@gmail.com>
2022-11-11 16:26:10 +0530

    added directives

--- app1.js
+++ app1.js
@@ -13,9 +13,16 @@ nApp.controller('mController',['$scope','$filter',function($scope,$filter){
     $scope.lowerhandle = function(){
         return $filter('lowercase')($scope.handle);
     };
-    $scope.$watch('handle',function(newValue,oldValue){
-        console.log('Changed');
-        console.log('old' +oldValue);
-        console.log('new'+newValue);
-    })
+    // $scope.$watch('handle',function(newValue,oldValue){
+    //     console.log('Changed');
+    //     console.log('old' +oldValue);
+    //     console.log('new'+newValue);
+    // })
+    $scope.characters = 5;
+
+    $scope.rules = [
+        {rulename: "Must be 5 characters"},
+        {rulename: "Must not be used elsewhere "},
+        {rulename: "Must be cool"}
+    ];
 }]);
